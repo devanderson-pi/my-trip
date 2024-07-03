0a43190c4e9bb5a15e66f68b01accc774594153e
anderson pires <devanderson.pi@gmail.com>
2024-07-02 22:41:03 -0300

    🔧 Update i18next file

--- src/config/i18next.ts
+++ src/config/i18next.ts
@@ -1,14 +1,18 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { ReactNativeLanguageDetector } from 'react-native-localization-settings';
 
 import resources from '../data/translations';
 
-i18next.use(initReactI18next).init({
-  compatibilityJSON: 'v3',
-  fallbackLng: 'en-US',
-  interpolation: {
-    escapeValue: false,
-  },
-  resources,
-  supportedLngs: ['en-US', 'pt-BR'],
-});
+i18next
+  .use(ReactNativeLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    compatibilityJSON: 'v3',
+    fallbackLng: 'en-US',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources,
+    supportedLngs: ['en-US', 'pt-BR'],
+  });
